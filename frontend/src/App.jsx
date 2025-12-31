b8118bac02b37890eccc00554e3ecbfd808da2e2
samarasimhareddy <samarasimha444@gmail.com>
2026-01-01 00:24:53 +0530

    everything id fine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,52 @@
-
-import { Client } from "@stomp/stompjs";
-export default function App() {
+import { useEffect, useState } from "react";
 
 
-
-const client = new Client({
-  brokerURL: "wss://localhost:8080/ws",
-  onConnect: () => {
-    console.log("✅ STOMP CONNECTED");
-  },
-  onStompError: (frame) => {
-    console.error("❌ STOMP ERROR", frame);
-  },
-  debug: (msg) => console.log("🐛 STOMP:", msg),
-});
-
-client.activate();
+export default function Dashboard() {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
 
 
-return(
-  <div>samara</div>
-)
+  useEffect(() => {
+    fetch("https://localhost:8080/me", {
+      credentials: "include", // 🔥 sends JWT cookie
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error("Not authenticated");
+        return res.json();
+      })
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((err) => {
+       //navigate to https://localhost:8080/oauth2/authorization/google
+       window.location.href = "https://localhost:8080/oauth2/authorization/google";
+      });
+  }, []);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!user) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+
+      <img
+        src={user.picture}
+        
+        width={80}
+        height={80}
+        style={{ borderRadius: "50%" }}
+      />
+
+      <p><b>ID:</b> {user.id}</p>
+      <p><b>Name:</b> {user.name}</p>
+      <p><b>Email:</b> {user.email}</p>
+      <p>{user.picture}</p>
+    </div>
+  );
 }
